b366d017be86c94b540d7b7f8eb6fb983c09e6df
Ligadema <49647254+Ligadema@users.noreply.github.com>
2019-04-22 20:41:34 +0300

    Update Main.java

--- src/by/yandex/sidvlada/Main.java
+++ src/by/yandex/sidvlada/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Task1 task1 = new Task1();
-        //task1.Date();
+        Task1 task1 = new Task1();
+        task1.Date();
 
-        //Task2 task2 = new Task2();
-        //task2.Houses();
+        Task2 task2 = new Task2();
+        task2.Houses();
 
         Task3 task3 = new Task3();
         task3.Factorial();
